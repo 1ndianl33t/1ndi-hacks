0a0d4ccf0e5df49a11589eef1edd6f8d3b440d2c
Shiv Chouhan <shivchouhan679@gmail.com>
2020-05-26 19:19:17 +0000

    status. code check

---
+++ 1ndi-status/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+        "bufio"
+        "fmt"
+        "github.com/tomnomnom/gahttp"
+        "net/http"
+        "os"
+        "time"
+)
+
+func printStatus(req *http.Request, resp *http.Response, err error) {
+        if err != nil {
+                return
+        }
+        fmt.Printf("%s: %s\n", req.URL, resp.Status)
+}
+
+func main() {
+        p := gahttp.NewPipeline()
+        p.SetConcurrency(20)
+        p.SetRateLimit(time.Second * 1)
+        urls := gahttp.Wrap(printStatus, gahttp.CloseBody)
+        sc := bufio.NewScanner(os.Stdin)
+        for sc.Scan() {
+                p.Get(sc.Text(), urls)
+        }
+        
+        p.Done()
+
+        p.Wait()
+}
