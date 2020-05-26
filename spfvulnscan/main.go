ffea9478a2ddda9fcb329a539f601e7b6b103976
Shiv Chouhan <shivchouhan679@gmail.com>
2020-05-26 08:36:34 +0000

    Create main.go

---
+++ spfvulnscan/main.go
@@ -0,0 +1,19 @@
+package main
+ 
+import (
+	      "fmt"
+        "net"
+        "bufio"
+        "os"
+)
+
+func main() {
+         sc := bufio.NewScanner(os.Stdin)
+        for sc.Scan() {
+        domain := sc.Text()
+	txtrecords, _ := net.LookupTXT(domain)
+ 
+	for _, txt := range txtrecords {
+		fmt.Println(txt)
+	}
+}
