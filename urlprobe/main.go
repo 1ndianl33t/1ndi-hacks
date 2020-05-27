d8e9564f7e765429604fd471b912168bd3fbab58
Shiv Chouhan <shivchouhan679@gmail.com>
2020-05-27 09:23:15 +0000

    Create main.go

---
+++ urlprobe/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+        "bufio"
+        "fmt"
+        "github.com/tomnomnom/gahttp"
+        "net/http"
+        "os"
+        "time"
+)
+import (
+        "bufio"
+        "fmt"
+        "github.com/fatih/color"
+        "github.com/tomnomnom/gahttp"
+        "net/http"
+        "os"
+        "time"
+)
+
+func Banner() {
+        color.HiCyan(`
+ ____ _____________.____   __________              ___.
+|    |   \______   \    |  \______   \_______  ____\_ |__   ____
+|    |   /|       _/    |   |     ___/\_  __ \/  _ \| __ \_/ __ \
+|    |  / |    |   \    |___|    |     |  | \(  <_> ) \_\ \  ___/
+|______/  |____|_  /_______ \____|     |__|   \____/|___  /\___  >
+                 \/        \/                           \/     \/   V 1.0`)
+
+        color.HiYellow("URLProbe:- Urls Status Code Checker")
+
+        color.HiRed("https://github.com/1ndianl33t")
+}
+func printStatus(req *http.Request, resp *http.Response, err error) {
+        if err != nil {
+                return
+        }
+        fmt.Printf("[%d] : %s\n", resp.StatusCode, req.URL)
+}
+
+func main() {
+        Banner ()
+        p := gahttp.NewPipeline()
+        p.SetConcurrency(20)
+        p.SetRateLimit(time.Second * 10)
+        urls := gahttp.Wrap(printStatus, gahttp.CloseBody)
+        sc := bufio.NewScanner(os.Stdin)
+        for sc.Scan() {
+                p.Get(sc.Text(), urls)
+        }
+        
+        p.Done()
+
+        p.Wait()
+}
