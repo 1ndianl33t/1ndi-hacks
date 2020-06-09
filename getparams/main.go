b31c1e13444f2b594a7ce19de30dbc8b117cca43
Shiv Chouhan <shivchouhan679@gmail.com>
2020-06-09 10:03:35 +0000

    Create main.go

---
+++ getparams/main.go
@@ -0,0 +1,23 @@
+ackage main
+
+import (
+        "bufio"
+        "fmt"
+        "net/url"
+        "os"
+)
+
+func main() {
+        sc := bufio.NewScanner(os.Stdin)
+        for sc.Scan() {
+                u, err := url.Parse(sc.Text())
+                if err != nil {
+                        //fmt.Fprintf(os.Stderr, "failed to parse url %s [%s]\n", sc.Text(), err)
+                }
+                m, _ := url.ParseQuery(u.RawQuery)
+
+                for key, _ := range m {
+                        fmt.Println(key)
+                }
+        }
+}
