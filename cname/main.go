21ee89f6a7e990d85cc0b9a9c560fbfa18f6c0a2
Shiv Chouhan <shivchouhan679@gmail.com>
2020-06-27 08:54:31 +0000

    added single domain

--- cname/main.go
+++ cname/main.go
@@ -1,17 +1,37 @@
 package main
 
-import (                                                                                
+import (
+        "bufio"
+        "flag"
         "fmt"
         "net"
-        "bufio"
         "os"
 )
 
 func main() {
-         sc := bufio.NewScanner(os.Stdin)
-        for sc.Scan() {
-        domain := sc.Text()
-        cname, _ := net.LookupCNAME(domain)
-        fmt.Println(cname)
- }
+        var domains []string
+
+        flag.Parse()
+        if flag.NArg() > 0 {
+                domains = []string{flag.Arg(0)}
+
+        } else {
+
+                sc := bufio.NewScanner(os.Stdin)
+                for sc.Scan() {
+                        domains = append(domains, sc.Text())
+                }
+
+                if err := sc.Err(); err != nil {
+                        fmt.Fprintf(os.Stderr, "failed to read input: %s\n", err)
+                }
+
+        }
+        for _, fetch := range domains {
+                cname, _ := net.LookupCNAME(fetch)
+                fmt.Printf("%s            ", fetch)
+                fmt.Printf("%s\n ", cname)
+        }
+        fmt.Printf("\n")
+
 }
